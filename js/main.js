670b278aed87436fb743968969d807cd8379f8de
Roman <romas6ka@i.ua>
2020-02-14 16:41:03 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(function() {
+  $("#slider").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: true,
+    dotsClass: "myDots",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        settings: {
+          arrows: true
+        }
+      }
+    ]
+  });
+
+  $("#sliderTestimonials").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    dotsClass: "myDots"
+  });
+
+  $("#btnOpen").on("click", function() {
+    $("#menu").slideToggle();
+  });
+});
+
+if (location.host !== "rogue-chefs.matviy.pp.ua") {
+  document.body.innerHTML = "";
+}
